464eacd6f5d2498a8b8605069d86b29eeabcd033
artemiydelaetkrasivo <hinzmillerpssdm2@gmail.com>
2025-08-31 00:07:57 +0300

    Add method check for send_to_telegram.php

--- send_to_telegram.php
+++ send_to_telegram.php
@@ -1,7 +1,16 @@
+
 <?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405);
+  echo 'Method Not Allowed';
+  exit;
+}
+
 $token = "8302933612:AAFvows2LPMl-PMLHtuq9fpv5gaAiXmPMlw";
 $chat_id = "1688309804";
 $text = $_POST['text'];
-file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($text));
+
+$result = file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=" . urlencode($text));
+
 echo "ok";
 ?>
